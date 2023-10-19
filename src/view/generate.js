48cd2b2d6fd0bee2542d35c6849c1a84fb850f7c
Avery Smets <avery@districtthree.be>
2023-10-19 17:11:33 +0200

    List last five selected folders

--- src/view/generate.js
+++ src/view/generate.js
@@ -24,6 +24,21 @@ const folderSelectionInput = document.getElementById('folder-selection');
 
 folderSelectionInput.addEventListener('click', async (event) => {
   const folder = await folderDialog.open();
+  
+  const recentFolders = JSON.parse(localStorage.getItem('recentFolders')) || [];
+  
+  //Push the new folder into the array
+  recentFolders.push(folder);
+
+  //Remove duplicates from the array
+  const uniqueFolders = [...new Set(recentFolders)];
+
+  //Slice the array to only hold the latest 5 folders
+  const lastFiveFolders = uniqueFolders.slice(Math.max(uniqueFolders.length - 5, 0));
+  
+  //Save the recent folders to localStorage
+  localStorage.setItem('recentFolders', JSON.stringify(lastFiveFolders));
+
   updateFolder(folder);
   event.preventDefault();
 });
@@ -48,6 +63,33 @@ projectDescriptionTextarea.addEventListener('input', async () => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
+  
+  const recentFolders = JSON.parse(localStorage.getItem('recentFolders')) || [];
+  const recentFoldersList = document.getElementById('recent-folders-list');
+  const loadFolderButton = document.getElementById('load-folder-button');
+
+  
+  //Populate the list with the recentFolder
+  recentFolders.forEach(folder => {
+    const listItem = document.createElement('li');
+    listItem.textContent = folder;
+    listItem.addEventListener('click', () => {
+      listItem.setAttribute('selected', 'selected');
+    });
+    recentFoldersList.appendChild(listItem);
+  });
+
+  //Add event listener to the loadFolderButton
+  loadFolderButton.addEventListener('click', () => {
+    const selectedListItems = Array.from(recentFoldersList.getElementsByTagName('li')).filter(listItem => listItem.hasAttribute('selected'));
+    if(selectedListItems.length > 0) {
+      const folderToLoad = selectedListItems[0].innerText;
+      updateFolder(folderToLoad);
+    } else {
+      console.log('No folder selected');
+    }
+  });
+
   // Make the fileListController global so it can be accessed in Agent.js
   window.fileListController = new FileListController(); 
   const fileListElement = window.fileListController.createDOM();
@@ -55,3 +97,4 @@ window.addEventListener('DOMContentLoaded', () => {
   
   fileListContainer.appendChild(fileListElement);
 });
+
