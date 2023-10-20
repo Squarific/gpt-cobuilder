7a78dbfaf268143e48626bee4eca370b62420d64
Avery Smets <avery@districtthree.be>
2023-10-20 15:15:55 +0200

    Move agents into directory

--- src/view/agents.js
+++ src/view/agents.js
@@ -1,12 +1,20 @@
-let agents;
+let agents = [];
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const agentsFilePath = `gptcobuilder/agents.json`;
-    const agentsFile = await window.fs.readFile(agentsFilePath);
-    
-    agents = JSON.parse(agentsFile).agents;
-    agents = agents.map(agentData => new Agent(agentData));
-    agents.forEach(agent => agent.createTab());
+  const agentsDirPath = `gptcobuilder/agents/`;
+  const agentsFiles = await window.fs.readdir(agentsDirPath);
+  
+  // Load each agent file
+  for (let i = 0; i < agentsFiles.length; i++) {
+    let agentFilePath = `${agentsDirPath}${agentsFiles[i]}`;
+    const agentFile = await window.fs.readFile(agentFilePath);
+  
+    let agentData = JSON.parse(agentFile);
+    let agent = new Agent(agentData);
+
+    agents.push(agent);
+    agent.createTab();
+  }
 });
 
 window.addEventListener('DOMContentLoaded', () => {
