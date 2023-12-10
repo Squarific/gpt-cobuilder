e50753bee4123a410ba864711a6ab84c399e494a
Avery Smets <avery@districtthree.be>
2023-12-10 19:57:15 +0100

    Add token usage, cost calculation, and request/response content to requests table

--- src/view/RequestLoader.js
+++ src/view/RequestLoader.js
@@ -23,10 +23,48 @@ class RequestLoader {
 
         const cellStatus = row.insertCell();
         cellStatus.textContent = 'DONE'; // Since all requests are considered DONE for now
+
+        // Additional data columns
+        const cellInputTokens = row.insertCell();
+        cellInputTokens.textContent = requestLog.response.usage.prompt_tokens;
+
+        const cellCompletionTokens = row.insertCell();
+        cellCompletionTokens.textContent = requestLog.response.usage.completion_tokens;
+
+        const cellCost = row.insertCell();
+        const cost = this.calculateCostFromResponse(requestLog.response);
+        cellCost.textContent = `$${cost}`;
+
+        const cellFinishReason = row.insertCell();
+        cellFinishReason.textContent = requestLog.response.choices[0].finish_reason;
+
+        const cellRequestContent = row.insertCell();
+        cellRequestContent.textContent = requestLog.request.content.substring(0, 100) + "...";
+
+        const cellResponseContent = row.insertCell();
+        cellResponseContent.textContent = requestLog.response.choices[0].message.content.substring(0, 100) + "...";
       });
     } catch (error) {
       console.error('Error loading requests:', error);
     }
+  }
+
+  calculateCostFromResponse(response) {
+    const model = response.model;
+    const inputTokens = response.usage.prompt_tokens;
+    const outputTokens = response.usage.completion_tokens;
+    // Let's assume these costs for the example - adjust according to the actual costs
+    let inputTokenCost = 0.02; // Cost per 1k input tokens
+    let outputTokenCost = 0.06; // Cost per 1k output tokens
+
+    if (model.includes("gpt-4")) {
+      // Adjust costs for gpt-4 model
+      inputTokenCost = 0.03;
+      outputTokenCost = 0.09;
+    }
+
+    const cost = (inputTokens * inputTokenCost / 1000) + (outputTokens * outputTokenCost / 1000);
+    return cost.toFixed(2); // Returns the cost with 2 decimal places
   }
 }
 
