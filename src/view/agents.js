5c4ad601e574115d974f7f4d64f6082fe836d678
Avery Smets <avery@districtthree.be>
2023-12-10 18:57:22 +0100

    Revert "Add Git status check and warning display functionality"
    This reverts commit ab6d92e136e0ebfae777726adbe5d6f23eff5233.

--- src/view/agents.js
+++ src/view/agents.js
@@ -1,5 +1,3 @@
-const { exec } = require('child_process');
-
 let agents = [];
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -17,56 +15,4 @@ window.addEventListener('DOMContentLoaded', async () => {
     agents.push(agent);
     agent.createTab();
   }
-
-  const folderPath = localStorage.getItem('folder');
-  const hasChanges = await checkGitStatus(folderPath);
-
-  if (hasChanges) {
-    displayGitWarning(folderPath);
-  }
-  
 });
-
-async function checkGitStatus(folderPath) {
-  return new Promise((resolve) => {
-    exec(`git -C ${folderPath} status --porcelain`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      
-      // If stdout has content, it means there are changes
-      if (stdout) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-  });
-}
-
-function displayGitWarning(folderPath) {
-  const gitWarningContainer = document.getElementById('git-warning-container');
-  const warningMessage = document.createElement('p');
-  warningMessage.textContent = 'There are uncommitted changes';
-  warningMessage.style.color = 'red';
-
-  const commitButton = document.createElement('button');
-  commitButton.textContent = 'Generate commit message and commit and push changes';
-  commitButton.addEventListener('click', async () => {
-    // Trigger the GitMasterAgent to create a commit message
-    const GitMasterAgent = agents.find((agent) => agent.data.name === 'Git Master');
-    const response = await GitMasterAgent.run();
-    
-    // Commit and push changes
-    await gitOperations();
-  });
-
-  gitWarningContainer.appendChild(warningMessage);
-  gitWarningContainer.appendChild(commitButton);
-}
-
