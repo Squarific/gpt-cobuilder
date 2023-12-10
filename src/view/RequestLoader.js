a1679802eaa57239d334d159fb4da29ec652ca90
Avery Smets <avery@districtthree.be>
2023-12-10 19:50:12 +0100

    Revert "Add content, token details, cost and finish reason to request logs with collapsible view"
    This reverts commit 588e41e16605f93382c9d92b913b0bc975baf785.

--- src/view/RequestLoader.js
+++ src/view/RequestLoader.js
@@ -1,9 +1,4 @@
 class RequestLoader {
-  constructor() {
-    // Accessing the calculateCost function from apiOperations
-    this.calculateCost = window.calculateCost;  
-  }
-
   async loadRequests() {
     const folder = localStorage.getItem('folder');
     const requestsPath = `${folder}/gptcobuilder/requests`;
@@ -18,65 +13,20 @@ class RequestLoader {
 
         const row = table.insertRow();
 
+        const cellAgentName = row.insertCell();
+        cellAgentName.textContent = requestLog.response.model;
+
         // Extract the date/time from the file name by removing the file extension first
         const dateTimeString = fileName.split('.')[0].replace(/-/g, ':');
         const cellDateTime = row.insertCell();
         cellDateTime.textContent = new Date(dateTimeString).toLocaleString();
 
-        const cellAgentName = row.insertCell();
-        cellAgentName.textContent = requestLog.response.model;
-
         const cellStatus = row.insertCell();
         cellStatus.textContent = 'DONE'; // Since all requests are considered DONE for now
-
-        const requestContent = requestLog.request.content;
-        const responseContent = requestLog.response.choices[0].message.content;
-        const finishReason = requestLog.response.choices[0].finish_reason;
-        const tokensCount = requestLog.response.usage;
-        const cost = this.calculateCost(tokensCount.prompt_tokens, tokensCount.completion_tokens, requestLog.response.model);
-        
-        const cellContent = row.insertCell();
-        const collapsibleSection = this.createCollapsibleSection('View Details', `Request: ${requestContent}\n\nResponse: ${responseContent}`, tokensCount, finishReason, cost);
-        cellContent.appendChild(collapsibleSection);
       });
     } catch (error) {
       console.error('Error loading requests:', error);
     }
-  }
-  
-  createCollapsibleSection(title, content, count, reason, cost) {
-    const container = document.createElement('div');
-
-    const button = document.createElement('button');
-    button.className = 'collapsible';
-    button.innerText = title;
-    container.appendChild(button);
-
-    const contentDiv = document.createElement('div');
-    contentDiv.className = 'collapsible-content';
-
-    const pre = document.createElement('pre');
-    pre.textContent = content;
-    contentDiv.appendChild(pre);
-
-    const infoDiv = document.createElement('div');
-    infoDiv.innerHTML = `<strong>Input Tokens:</strong> ${count.prompt_tokens}, <strong>Completion Tokens:</strong> ${count.completion_tokens}, <strong>Total Tokens:</strong> ${count.total_tokens}, <strong>Cost:</strong> $${cost.toFixed(2)}, <strong>Finish Reason:</strong> ${reason}`;
-    contentDiv.appendChild(infoDiv);
-
-    container.appendChild(contentDiv);
-
-    // Add event listener to toggle collapsible content
-    button.addEventListener('click', function () {
-      this.classList.toggle('active');
-      const contentDiv = this.nextElementSibling;
-      if (contentDiv.style.display === 'none' || contentDiv.style.display === '') {
-        contentDiv.style.display = 'block';
-      } else {
-        contentDiv.style.display = 'none';
-      }
-    });
-
-    return container;
   }
 }
 
@@ -85,4 +35,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const requestLoader = new RequestLoader();
   requestLoader.loadRequests();
 });
-
