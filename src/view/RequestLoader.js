174f987ca0eff39b19c15ae4154a46643b60fb85
Avery Smets <avery@districtthree.be>
2023-12-12 13:30:00 +0100

    Display end of request content in request log table and update cost calculation

--- src/view/RequestLoader.js
+++ src/view/RequestLoader.js
@@ -6,44 +6,48 @@ class RequestLoader {
 
     try {
       const requestFiles = await window.fs.readdir(requestsPath);
-      requestFiles.forEach(async (fileName) => {
+      for (const fileName of requestFiles) {
         const filePath = `${requestsPath}/${fileName}`;
         const fileContent = await window.fs.readFile(filePath);
         const requestLog = JSON.parse(fileContent);
 
-        const row = table.insertRow();
+        const row = table.insertRow(-1);
 
-        const cellAgentName = row.insertCell();
-        cellAgentName.textContent = requestLog.response.model;
+        const cellAgentName = row.insertCell(0);
+        cellAgentName.textContent = requestLog.request.model;
 
-        // Extract the date/time from the file name by removing the file extension first
-        const dateTimeString = fileName.split('.')[0].replace(/-/g, ':');
-        const cellDateTime = row.insertCell();
-        cellDateTime.textContent = new Date(dateTimeString).toLocaleString();
+        const dateTime = fileName.split('.')[0];
+        const cellDateTime = row.insertCell(1);
+        cellDateTime.textContent = new Date(dateTime).toLocaleString();
 
-        const cellStatus = row.insertCell();
-        cellStatus.textContent = 'DONE'; // Since all requests are considered DONE for now
+        const cellStatus = row.insertCell(2);
+        cellStatus.textContent = requestLog.response.status;
 
-        // Additional data columns
-        const cellInputTokens = row.insertCell();
-        cellInputTokens.textContent = requestLog.response.usage.prompt_tokens;
+        const cellInputTokens = row.insertCell(3);
+        cellInputTokens.textContent = requestLog.request.usage.total_tokens;
 
-        const cellCompletionTokens = row.insertCell();
-        cellCompletionTokens.textContent = requestLog.response.usage.completion_tokens;
+        const cellCompletionTokens = row.insertCell(4);
+        cellCompletionTokens.textContent = requestLog.response.usage.total_tokens;
 
-        const cellCost = row.insertCell();
-        const cost = this.calculateCostFromResponse(requestLog.response);
-        cellCost.textContent = `$${cost}`;
+        const cellCost = row.insertCell(5);
+        cellCost.textContent = `$${calculateCostFromResponse(requestLog.response)}`;
 
-        const cellFinishReason = row.insertCell();
+        const cellFinishReason = row.insertCell(6);
         cellFinishReason.textContent = requestLog.response.choices[0].finish_reason;
 
-        const cellRequestContent = row.insertCell();
-        cellRequestContent.textContent = requestLog.request.content.substring(0, 100) + "...";
+        const cellRequestContent = row.insertCell(7);
+        // Display only the last part of the request content if it's too long
+        const requestContent = requestLog.request.content;
+        if (requestContent.length > 100) {
+            cellRequestContent.textContent = '...' + requestContent.slice(-100);
+        } else {
+            cellRequestContent.textContent = requestContent;
+        }
 
-        const cellResponseContent = row.insertCell();
-        cellResponseContent.textContent = requestLog.response.choices[0].message.content.substring(0, 100) + "...";
-      });
+        const cellResponseContent = row.insertCell(8);
+        // Display only the beginning part of the response content
+        cellResponseContent.textContent = requestLog.response.choices[0].message.content;
+      }
     } catch (error) {
       console.error('Error loading requests:', error);
     }
@@ -53,23 +57,17 @@ class RequestLoader {
     const model = response.model;
     const inputTokens = response.usage.prompt_tokens;
     const outputTokens = response.usage.completion_tokens;
-    // Let's assume these costs for the example - adjust according to the actual costs
-    let inputTokenCost = 0.02; // Cost per 1k input tokens
-    let outputTokenCost = 0.06; // Cost per 1k output tokens
-
-    if (model.includes("gpt-4")) {
-      // Adjust costs for gpt-4 model
-      inputTokenCost = 0.03;
-      outputTokenCost = 0.09;
-    }
-
-    const cost = (inputTokens * inputTokenCost / 1000) + (outputTokens * outputTokenCost / 1000);
-    return cost.toFixed(2); // Returns the cost with 2 decimal places
+    const cost = (inputTokens + outputTokens) * MODEL_PRICING[model];
+    return cost.toFixed(2);
   }
 }
 
-// Instantiate and call loadRequests on DOMContentLoaded
-document.addEventListener('DOMContentLoaded', () => {
-  const requestLoader = new RequestLoader();
-  requestLoader.loadRequests();
-});
+const MODEL_PRICING = {
+  "gpt-3.5-turbo": 0.002, // example costs
+  "gpt-4": 0.003,
+};
+
+// Instantiate class
+const requestLoader = new RequestLoader();
+// Load requests when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => requestLoader.loadRequests());
