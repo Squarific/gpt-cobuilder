022b5812831873a437c1a6c342af9af0253d2974
Avery Smets <avery@districtthree.be>
2023-12-12 15:36:30 +0100

    Add pagination and display logic for request logs in PaginatedTable

--- src/view/PaginatedTable.js
+++ src/view/PaginatedTable.js
@@ -15,7 +15,7 @@ class PaginatedTable {
     try {
       const requestFiles = await window.fs.readdir(requestsPath);
       this.totalPages = Math.ceil(requestFiles.length / ITEMS_PER_PAGE);
-     
+
       this.updateTable(table, requestFiles);
       this.addPaginationControls();
     } catch (error) {
@@ -24,19 +24,61 @@ class PaginatedTable {
   }
 
   // Update display for the current page
-  updateTable(table, requestFiles) {
+  async updateTable(table, requestFiles) {
     // Only get files for the current page
     const start = (this.currentPage - 1) * ITEMS_PER_PAGE;
     const end = this.currentPage * ITEMS_PER_PAGE;
     const pageFiles = requestFiles.slice(start, end);
 
     table.innerHTML = ''; // clear table content
+    this.createTableHeaders(table); // Add table headers
+    await this.populateTableWithPageFiles(table, pageFiles);
+  }
 
-    // Generate the table with the sliced request logs
-    // Add your logic to insert the rows into the table
-    
-    // Add table headers 
-    // (...)
+  createTableHeaders(table) {
+    const headers = ["Agent Name", "Date/Time", "Status", "Input Tokens", "Completion Tokens", "Cost", "Finish Reason", "Request Content", "Response Content"];
+    const headerRow = table.insertRow();
+    for (const headerText of headers) {
+      const headerCell = headerRow.insertCell();
+      headerCell.textContent = headerText;
+    }
+  }
+
+  async populateTableWithPageFiles(table, pageFiles) {
+    const folder = localStorage.getItem('folder');
+    const requestsPath = `${folder}/gptcobuilder/requests`;
+
+    for (const fileName of pageFiles) {
+      const filePath = `${requestsPath}/${fileName}`;
+      const fileContent = await window.fs.readFile(filePath);
+      const requestLog = JSON.parse(fileContent);
+
+      const row = table.insertRow();
+      this.fillCell(row, requestLog.response.model);
+      this.fillCell(row, new Date(fileName.split('.')[0].replace(/-/g, ':')).toLocaleString());
+      this.fillCell(row, 'DONE');
+      this.fillCell(row, requestLog.response.usage.prompt_tokens);
+      this.fillCell(row, requestLog.response.usage.completion_tokens);
+      this.fillCell(row, `$${this.calculateCostFromResponse(requestLog.response)}`);
+      this.fillCell(row, requestLog.response.choices[0].finish_reason);
+
+      let requestContent = requestLog.request.content.slice(-256);
+      let responseContent = requestLog.response.choices[0].message.content.slice(0, 256);
+      
+      this.fillCell(row, requestContent);
+      this.fillCell(row, responseContent);
+    }
+  }
+
+  fillCell(row, text) {
+    const cell = row.insertCell();
+    cell.textContent = text;
+  }
+
+  calculateCostFromResponse(response) {
+    // Assume you have some logic to calculate cost based on response tokens
+    const cost = response.usage.total_tokens * 0.0006; // Example cost calculation
+    return cost.toFixed(2);
   }
 
   // Add pagination controls below the table
@@ -55,7 +97,7 @@ class PaginatedTable {
     prevButton.disabled = this.currentPage <= 1;
     prevButton.addEventListener('click', () => this.changePage(-1));
     container.appendChild(prevButton);
-   
+
     // Add page number display
     const pageNumDisplay = document.createElement('span');
     pageNumDisplay.textContent = `Page ${this.currentPage} of ${this.totalPages}`;
@@ -74,12 +116,11 @@ class PaginatedTable {
   // Change to previous or next page
   changePage(direction) {
     this.currentPage += direction;
-
     this.loadTable(); // Reload the table content
   }
 }
 
-// Instantiate and call loadRequests on DOMContentLoaded
+// Instantiate and call loadTable on DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
   const paginatedRequestTable = new PaginatedTable('requests-table');
   paginatedRequestTable.loadTable();
