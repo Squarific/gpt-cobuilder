8b90e8e8e6b18d913a4b2b6bab629f280890fb81
Avery Smets <avery@districtthree.be>
2023-12-30 19:21:47 +0100

    Generate settings tab HTML elements dynamically in `SettingsTab.js`

--- src/view/SettingsTab.js
+++ src/view/SettingsTab.js
@@ -1,55 +1,100 @@
-const apiKeyInput = document.getElementById('api-key');
+const settingsTabContent = document.getElementById('Settings');
 
-// Load the API key from localStorage if it exists
-const savedApiKey = localStorage.getItem('apiKey');
-if (savedApiKey) {
-  apiKeyInput.value = savedApiKey;
+// Function to create and append settings elements
+function createSettingsElements() {
+  // API Key
+  const apiKeyLabel = document.createElement('label');
+  apiKeyLabel.setAttribute('for', 'api-key');
+  apiKeyLabel.textContent = 'ChatGPT API Key:';
+  settingsTabContent.appendChild(apiKeyLabel);
+
+  const apiKeyInput = document.createElement('input');
+  apiKeyInput.type = 'text';
+  apiKeyInput.id = 'api-key';
+  settingsTabContent.appendChild(apiKeyInput);
+
+  // Folder Selection
+  const folderSelectionLabel = document.createElement('label');
+  folderSelectionLabel.setAttribute('for', 'folder-selection');
+  folderSelectionLabel.textContent = 'Select a Folder:';
+  settingsTabContent.appendChild(folderSelectionLabel);
+
+  const folderSelectionButton = document.createElement('button');
+  folderSelectionButton.id = 'folder-selection';
+  folderSelectionButton.textContent = 'Project folder';
+  settingsTabContent.appendChild(folderSelectionButton);
+
+  // Model Selection
+  const modelSelectionLabel = document.createElement('label');
+  modelSelectionLabel.setAttribute('for', 'model-selection');
+  modelSelectionLabel.textContent = 'Select a model:';
+  settingsTabContent.appendChild(modelSelectionLabel);
+
+  const modelSelectionSelect = document.createElement('select');
+  modelSelectionSelect.id = 'model-selection';
+  const modelOptions = [
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-16k',
+    'gpt-4',
+    'gpt-4-32k',
+    'gpt-4-1106-preview',
+  ];
+  modelOptions.forEach(model => {
+    const option = document.createElement('option');
+    option.value = model;
+    option.textContent = model;
+    modelSelectionSelect.appendChild(option);
+  });
+  settingsTabContent.appendChild(modelSelectionSelect);
+  
+  // Folder Display
+  const folderDisplaySpan = document.createElement('span');
+  folderDisplaySpan.id = 'folder-display';
+  settingsTabContent.appendChild(folderDisplaySpan);
+
+  // Recent Folders List
+  const recentFoldersListUl = document.createElement('ul');
+  recentFoldersListUl.id = 'recent-folders-list';
+  settingsTabContent.appendChild(recentFoldersListUl);
 }
 
-//read saved folder from local storage
-updateFolder(localStorage.getItem('folder'));
+// Call createSettingsElements on DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
+  createSettingsElements();
+  addSettingsEventListeners();
+  updateRecentFolders();
 
-// Add event listener to the apiKeyInput field
-apiKeyInput.addEventListener('input', () => {
-  const apiKey = apiKeyInput.value;
-  // Save the API key in localStorage
-  localStorage.setItem('apiKey', apiKey.trim());
-});
-
-const folderSelectionInput = document.getElementById('folder-selection');
-
-folderSelectionInput.addEventListener('click', async (event) => {
-  const folder = await folderDialog.open();
-  
-  const recentFolders = JSON.parse(localStorage.getItem('recentFolders')) || [];
-  
-  recentFolders.push(folder);
-  const uniqueFolders = [...new Set(recentFolders)];
-  const lastFiveFolders = uniqueFolders.slice(Math.max(uniqueFolders.length - 5, 0));
-  localStorage.setItem('recentFolders', JSON.stringify(lastFiveFolders));
-
-  updateFolder(folder);
-  event.preventDefault();
-});
-
-function updateRecentFolders () {
-    const recentFolders = JSON.parse(localStorage.getItem('recentFolders')) || [];
-    const recentFoldersList = document.getElementById('recent-folders-list');
-  
-    recentFoldersList.textContent = "";
-  
-    recentFolders.forEach(folder => {
-      const listItem = document.createElement('li');
-      listItem.textContent = folder;
-      listItem.className = "button";
-      listItem.addEventListener('click', () => {
-        updateFolder(folder);
-        location = location;
-      });
-      recentFoldersList.appendChild(listItem);
-    });
+  // Load saved API key and folder
+  const savedApiKey = localStorage.getItem('apiKey');
+  if (savedApiKey) {
+    document.getElementById('api-key').value = savedApiKey;
   }
+  updateFolder(localStorage.getItem('folder'));
+});
 
-  window.addEventListener('DOMContentLoaded', async () => {
+// Function to add event listeners for settings elements
+function addSettingsEventListeners() {
+  const apiKeyInput = document.getElementById('api-key');
+  apiKeyInput.addEventListener('input', () => {
+    // Save the API key in localStorage
+    localStorage.setItem('apiKey', apiKeyInput.value.trim());
+  });
+
+  const folderSelectionInput = document.getElementById('folder-selection');
+  folderSelectionInput.addEventListener('click', async () => {
+    const folder = await folderDialog.open();
+    updateFolder(folder);
+    // Save and update recent folders
+    const recentFolders = JSON.parse(localStorage.getItem('recentFolders')) || [];
+    recentFolders.push(folder);
+    const uniqueFolders = [...new Set(recentFolders)];
+    localStorage.setItem('recentFolders', JSON.stringify(uniqueFolders.slice(-5)));
     updateRecentFolders();
   });
+
+  const modelSelection = document.getElementById('model-selection');
+  modelSelection.addEventListener('change', async () => {
+      const settings = { modelSelection: modelSelection.value };
+      await saveSettings(settings);
+  });
+}
